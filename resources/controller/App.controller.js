ec50d340636aadd3c13f9727944b3ee8c4c4fd3a
Hassan <hassan@delilounas.com>
2015-11-06 17:03:06 +0200

    asdf

---
+++ resources/controller/App.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define(
+	["sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"], 
+	function(Controller, JSONModel,MessageToast) {
+	"use strict";
+	return Controller.extend("controller.App", {
+		onInit:function  () {
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+
+		},
+		onShowHello:function(){
+			alert("Hello");
+		}
+
+	});
+});
